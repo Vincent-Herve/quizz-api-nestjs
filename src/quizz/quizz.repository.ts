d92a89afad3de85610f9e172219242074b2bce44
Vincent-Herve <vincent.herve2012@laposte.net>
2020-10-04 10:45:22 +0200

    clean master

--- src/quizz/quizz.repository.ts
+++
@@ -1,5 +0,0 @@
-import { Repository, EntityRepository } from "typeorm";
-import { Quizz } from './quizz.entity';
-
-@EntityRepository(Quizz)
-export class QuizzRepository extends Repository<Quizz> {}
